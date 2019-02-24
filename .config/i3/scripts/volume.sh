48e67efd29005e4031ad3bd9e358cdbaeacdc4f4
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-02-24 12:19:39 -0500

    initial i3 config

---
+++ .config/i3/scripts/volume.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://www.reddit.com/r/i3wm/comments/a0mgqn/how_can_i_make_my_volume keys_work_for_both_the/
+
+SINK=$(pacmd list-sinks | grep "index:" | grep '*' | awk '{print $3}')
+
+if [ "$SINK" == "" ]; then
+  SINK="0"
+fi
+
+if [ "$1" != "M" ]; then
+  pactl set-sink-volume "$SINK" "$1"
+else
+  pactl set-sink-mute "$SINK" toggle
+fi
+
